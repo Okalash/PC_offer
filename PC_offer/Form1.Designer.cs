1a862d5dfa026cef4b2c2cfea6b208e25ab44e82
Okalash <43669155+Okalash@users.noreply.github.com>
2019-10-10 15:33:12 +0300

    readonly text box with name

--- PC_offer/Form1.Designer.cs
+++ PC_offer/Form1.Designer.cs
@@ -61,6 +61,7 @@
             this.textBox1.ForeColor = System.Drawing.Color.White;
             this.textBox1.Location = new System.Drawing.Point(64, 12);
             this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 3;
             this.textBox1.Text = "PC_offer";
